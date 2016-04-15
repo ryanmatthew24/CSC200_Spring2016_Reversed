e06111a812decb861562d5c05a6aa04009861a8a
ryanmatthew24 <ryan.rogers@cuw.edu>
2016-04-15 00:05:40 -0500

    Update Driver.java

--- Driver Project/Driver.java
+++ Driver Project/Driver.java
@@ -3,9 +3,43 @@ public class Driver
 {
 	public static void main(String[] args)
 	{
-		String s = "hello";
-		String s2 = "elephant";
-		System.out.println(Driver.removeVowels(s));
+		String bin = "1011";
+		String s = "-124";
+		System.out.println(s + 5);
+		System.out.println(Driver.stringToInt(s) + 5);
+		System.out.println(Integer.parseInt(s) + 5);
+	}
+	
+	//this guy should take a String representation of a binary number
+	//as a parameter and return as a int the decimal equivalent
+	//"1011" -> 11
+	static int binaryToInteger(String bin)
+	{
+		
+		int place = 1;
+		int total = 0;
+		char currChar;
+		for(int i = bin.length()-1; i >= 0; i--)
+			{
+				currChar = bin.charAt(i);
+				if(currChar == '-')
+				{
+					return total * -1;
+				}
+				else
+				{
+					total += (Driver.charToInt(currChar) * place);
+					place *= 10;
+				}
+			}
+			return total;
+			int eights = total.charAt(0) * 8;
+			int fours = total.charAt(1) * 4;
+			int twos = total.charAt(2) * 2;
+			int ones = total.charAt(3) * 1;
+			return eights + fours + twos + ones;
+			
+		
 	}
 	
 	//return the integer version of the char parameter
@@ -19,9 +53,23 @@ public class Driver
 	//"124" -> 124
 	static int stringToInt(String s)
 	{
-		String stringNum = "0123";
-		int x = Integer.valueOf(stringNum);
-	    return x;
+		int place = 1;
+		int total = 0;
+		char currChar;
+		for(int i = s.length()-1; i >= 0; i--)
+		{
+			currChar = s.charAt(i);
+			if(currChar == '-')
+			{
+				return total * -1;
+			}
+			else
+			{
+				total += (Driver.charToInt(currChar) * place);
+				place *= 10;
+			}
+		}
+		return total;
 	}
 	
 	static String removeChars(String s, String charsToRemove)
