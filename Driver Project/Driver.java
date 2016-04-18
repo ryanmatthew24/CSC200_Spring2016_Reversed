c3a92d82b9fbd7fe09a89d5cfb89e4a14cb10897
ryanmatthew24 <ryan.rogers@cuw.edu>
2016-04-18 12:21:13 -0500

    Update Driver.java

--- Driver Project/Driver.java
+++ Driver Project/Driver.java
@@ -9,25 +9,138 @@ public class Driver
 	//integer into char 10->A
 	static char intToChar(int val)
 	{
-		char c = (char) ('0' + val);
-		"0123456789ABCEF".indexOf(c);
-		return c;
+		String map = "0123456789ABCDEF";
+		return map.charAt(val);
 	}
 	
 	static String decimalToBase(int decimalNumber, int radix)
 	{
 		//remember remainder and the division
-		String recordRemainder = ("") + decimalNumber % radix;
-		int recordNumber = decimalNumber / radix;
-		String answer = recordRemainder;
-	
-		while(decimalNumber > 0);
+		String answer = "";
+		
+		while(decimalNumber != 0)
 		{
-			Driver.intToChar(decimalNumber);
-			decimalNumber%=radix;
-			recordRemainder += decimalNumber%=radix;
-			decimalNumber/=radix;
-			recordNumber/=radix;
+			answer = Driver.intToChar(decimalNumber % radix) + answer;
+			decimalNumber /= radix;
 		}
         return answer;
+	}
+	
+		
+	
+	
+	
+	//return the integer version of the char parameter
+	
+	static int charToInt(char c)
+	{
+		return "0123456789ABCEF".indexOf(c);
+	}
+	
+	//Converts s, which is a string representation
+	//of an int into an int representation
+	//"124" -> 124
+	static int stringToInt(String s)
+	{
+		int place = 1;
+		int total = 0;
+		char currChar;
+		for(int i = s.length()-1; i >= 0; i--)
+		{
+			currChar = s.charAt(i);
+			if(currChar == '-')
+			{
+				return total * -1;
+			}
+			else
+			{
+				total += (Driver.charToInt(currChar) * place);
+				place *= 10;
+			}
+		}
+		return total;
+	}
+	
+	static String removeChars(String s, String charsToRemove)
+	{
+		String answer = "";
+		for(int i = 0; i < s.length(); i++)
+		{
+			//should we keep the current character
+			if(charsToRemove.indexOf(s.charAt(i)) == -1)
+			{
+				answer = answer + s.charAt(i);
+			}
+		}
+		return answer;
+	}
+	
+	//returns a new String with all of the vowels
+	//removed form the parameter String
+	//"hello" -> hll
+	static String removeVowels(String s)
+	{
+		return Driver.removeChars(s, "aeiouAEIOU");
+	}
+	
+	//returns a new String with all of the consonants
+	//removed form the parameter String
+	//"hello" -> eo
+	static String removeConsonants(String s)
+	{
+		return Driver.removeChars(s, "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ");
+	}
+		
+	//returns a new String with all of the numbers
+	//removed form the parameter String
+	//"he11o" -> heo
+	static String removeDigits(String s)
+	{
+		return Driver.removeChars(s, "0123456789");
+	}
+		
+	//return the first occurence in s where c is found or -1 if 
+	//it was not found - we can't use the String's indexOf
+	static int indexOf(String s, char c)
+	{
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			if(s.charAt(i) == c)
+			{
+				return i;
+			}
+		}
+		//if we are still kicking....
+		return -1;
+	}
+	
+	static void displayString(String s)
+	{
+		//should display the contents of the String one character per line
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			System.out.println(s.charAt(i));
+		}
+	}
+	
+	static void displayWithSpaces(String s)
+	{
+		String answer = "";
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			answer = answer + s.charAt(i) + " ";
+		}
+		System.out.println(answer);
+	}
+	
+	//this method should display on a single line the input String
+	//in reverse: "hello" -> "olleh"
+	static String reverseString(String s)
+	{
+		String answer = "";
+		for(int i = s.length()-1; i >= 0; i = i - 1)
+		{
+			answer = answer + s.charAt(i);
+		}
+		return answer;
 	}
